0d2af5f064e0419d4ff43c1ef920c0b93c8ba677
Abitamim <abitamimbharmal@hotmail.com>
2020-11-28 18:39:10 -0500

    second try

--- Github/Github-Runner.py
+++ Github/Github-Runner.py
@@ -1,8 +1,8 @@
 from pyfcm import FCMNotification
 
 
-my_api_key = "AIzaSyDjCRTvcoY_wUnh4eC6iqSOphLf_7vgpiE"
-my_registration_id = "AAAA0LI0lc8:APA91bFszMEf9jEIrN-fsY5gBKLTm5OVlruqaoylfu-7m2XY15_oneSlOKdqEHxosK1af4-wQWnEGnWoE-dNPd7efKzSHhFrFrvq4kNP8PUrcj5CM8FwyF2T8hDTZ99g0QnQBqGKQIDe"
+my_api_key = "AAAA0LI0lc8:APA91bFszMEf9jEIrN-fsY5gBKLTm5OVlruqaoylfu-7m2XY15_oneSlOKdqEHxosK1af4-wQWnEGnWoE-dNPd7efKzSHhFrFrvq4kNP8PUrcj5CM8FwyF2T8hDTZ99g0QnQBqGKQIDe"
+my_registration_id = "dL6pNRnkf4U:APA91bECg7PIqeq4zvu7x3aPfqJssODKCAyQC_NKi_5jBV1c9gx8r295wHrdvyU567sqhjs0HGsd5yRzDoRby7iuEAUrqXoHYexyHyHvPwUjgK2W0q4mvK3CwmqT2ADpIN5nxFVVVxN6"
 
 #uploaded to github and therefore insecure because it doesn't matter here
 push_service = FCMNotification(api_key=my_api_key)
